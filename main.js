8011db012a9b8d4f629e95502b698c1109719f93
arielmell0 <arielmello@rede.ulbra.br>
2022-02-01 20:03:47 -0300

    feat: funcao broadcast
    responsavel por enviar o que for digitado por qualquer usuario aos demais que tambem estao conectados. usei um filtar para nao duplicar a mensagem para quem acabou de enviar

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const net = require('net')
 const chatServer = net.createServer()
 const clientList = []
+
+
+const broadcast = (message, client) => {
+    clientList
+        .filter(item => item !== client)
+        .forEach(item => item.write(message))
+}
